f3d4d8e0b9a15adab683dfac6a51b63a203e923d
Marcos Dias <marcosaurelioce@hotmail.com>
2026-03-21 19:41:58 -0300

    feat: initial spring boot setup

---
+++ src/main/java/com/marcosdias/miniifood/MiniIfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcosdias.miniifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniIfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniIfoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosdias/miniifood/MiniIfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdias.miniifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniIfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
